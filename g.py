6d2e338bc422b749d77e99b7a726c1ed6b7d0153
Kousha <150776406+KoushaZhiyani@users.noreply.github.com>
2025-01-15 20:56:59 +0330

    Add files via upload

---
+++ g.py
@@ -0,0 +1,357 @@
+import numpy as np
+import pandas as pd
+from collections import defaultdict
+import warnings
+import copy
+import time
+import os
+srt_time = time.time()
+warnings.filterwarnings('ignore')
+
+
+class UnitSelection:
+    def __init__(self, data, flag=False, id=0, method=1, day_coff=10, gap_coff=10, pr_coff=10):
+        self.dt = data
+        self.base_table = np.zeros(shape=(6, 6))
+        self.df_matrix = pd.DataFrame()
+        self.c = 0
+        self.df_matrix_course = pd.DataFrame()
+        self.flag = flag
+        self.id = id
+        self.method = method
+
+        # اگر متد 2 انتخاب شده باشد، مقادیر پیش‌فرض استفاده می‌شوند
+        if method == 2:
+            self.day_coff = day_coff or 10
+            self.gap_coff = gap_coff or 10
+            self.pr_coff = pr_coff or 10
+        else:
+            self.day_coff, self.gap_coff, self.pr_coff = 10, 10, 10
+
+        self.select_method(method=method, day_coff=self.day_coff, gap_coff=self.gap_coff, pr_coff=self.pr_coff)
+        self.main_run()
+        self.values_dict = None
+
+    def select_method(self, method=1, day_coff=10, gap_coff=10, pr_coff=10):
+        if not self.flag:
+            self.day_coff = day_coff
+            self.gap_coff = gap_coff
+            self.pr_coff = pr_coff
+
+    def main_run(self):
+        self.dt = self.dt.apply(self.fill_score, axis=1)
+        self.dt['درس'] = self.index_trim(self.dt['درس'])
+        self.values_dict = self.values_dict_gen(self.dt)
+        self.selector(self.values_dict)
+        if not self.flag:
+            self.df_matrix.to_csv('all-matrix12.csv', index=False, encoding='utf-8-sig')
+            self.df_matrix_course.to_csv('all-record12.csv', index=False, encoding='utf-8-sig')
+            sorted_list = self.evaluate_matrix()
+            self.print_matrix(sorted_list)
+        else:
+            path_name = f'{self.id}-all-situations.csv'
+            self.df_matrix_course.to_csv(path_name, index=False, encoding='utf-8-sig')
+            print('*'*100)
+            print(f'All situations created. \nFile name: {path_name} \nFile path: {os.path.abspath(path_name)}')
+            print('*'*100)
+
+
+
+
+
+
+    def fill_score(self, row):
+        if pd.isna(row['امت?از']):
+            dt_gp = self.dt.groupby(by='درس').agg({'امت?از': 'mean'})
+            if not pd.isna(dt_gp['امت?از'][dt_gp.index == row['درس']].values[0]):
+                row['امت?از'] = dt_gp['امت?از'][dt_gp.index == row['درس']].values[0]
+            else:
+                row['امت?از'] = 5
+        return row
+
+
+    @staticmethod
+    def values_dict_gen(data):
+        values_dict = defaultdict(list)
+        for indx, row in data.iterrows():
+            values_dict[row[0]].append((row[1], row[2], row[3]))
+        return values_dict
+
+
+    @staticmethod
+    def standardize_persian_text(text, flag=True):
+        if flag:
+            return text[0].replace('ي', 'ی').replace('ك', 'ک').replace('?', 'ی')
+        return text.replace('ي', 'ی').replace('ك', 'ک').replace('?', 'ی')
+
+
+    def trim_query(self, query):
+        query_split = query.split(' ')
+        query_drop_space = [i for i in query_split if i]
+
+        cleaned_data = []
+        for i in range(len(query_drop_space)):
+            if not (query_drop_space[i] == 'از' and i > 0 and query_drop_space[i - 1] == 'از'):
+                cleaned_data.append(query_drop_space[i])
+
+        return cleaned_data
+
+
+    @staticmethod
+    def convert_day_to_int(query):
+        map_day = {
+            'شنبه': 0,
+            'یکشنبه': 1,
+            'دوشنبه': 2,
+            'سه': 3,
+            'چهارشنبه': 4,
+            'پنج': 5
+        }
+        query_edit = []
+        for i in query:
+            if i in map_day:
+                query_edit.append(map_day[i])
+            else:
+                query_edit.append(i)
+        if query_edit[1] == 0:
+            del query_edit[1]
+        if len(query_edit) > 6:
+            if query_edit[6] == 0:
+                del query_edit[6]
+        return query_edit
+
+
+    @staticmethod
+    def convert_time_to_int(time_str):
+        hour = time_str.split(':')[0]
+        hour_int = int(hour)
+
+        return hour_int
+
+
+    def time_series(self, query):
+        if len(query) > 8:
+            index = [2, 4, 7, 9]
+        else:
+            index = [2, 4]
+        sample = []
+
+        for i in index:
+            sample.append(self.convert_time_to_int(query[i]))
+
+        return sample
+
+
+    def check_class(self, query, hours):
+
+        if len(query) > 8:
+            day = [query[0], query[5]]
+        else:
+            day = [query[0]]
+        for i in range(0, len(hours), 2):
+
+            if hours[i + 1] - hours[i] == 2:
+                q = (hours[i] - 8) / 2
+                if self.base_table[day[0]][int(q)] + 1 == 1:
+                    day.pop(0)
+                    continue
+                else:
+                    return False
+            else:
+                q = (hours[i] - 8) / 2
+                if hours[i] % 2 == 0:
+                    if self.base_table[day[0]][int(q)] != 0.25 and self.base_table[day[0]][int(q)] + 0.25 <= 1:
+                        day.pop(0)
+                        continue
+                    else:
+                        return False
+                else:
+                    if self.base_table[day[0]][int(q)] != 0.75 and self.base_table[day[0]][int(q)] + 0.75 <= 1:
+                        day.pop(0)
+                        continue
+                    else:
+                        return False
+        return True
+
+
+    def trim_three_hours(self, start_time, day):
+        if start_time % 2 == 0:
+            return [day, 'از', self.convert_to_time_format(start_time), 'تا', self.convert_to_time_format(start_time + 2), day, 'از',
+                    self.convert_to_time_format(start_time + 2), 'تا', self.convert_to_time_format(start_time + 4)]
+        return [day, 'از', self.convert_to_time_format(start_time - 1), 'تا', self.convert_to_time_format(start_time), day, 'از',
+                self.convert_to_time_format(start_time), 'تا', self.convert_to_time_format(start_time + 2)]
+
+
+    @staticmethod
+    def convert_to_time_format(hour):
+        return str(hour) + ":00"
+
+
+    def tick_base_table(self, query, hours):
+        if len(query) > 8:
+            day = [query[0], query[5]]
+        else:
+            day = [query[0]]
+
+        if hours[1] - hours[0] == 3:
+            query = self.trim_three_hours(hours[0], query[0])
+        if self.check_class(query, hours):
+
+            for i in range(0, len(hours), 2):
+                if hours[i + 1] - hours[i] == 2:
+                    q = (hours[i] - 8) / 2
+                    self.base_table[day[0]][int(q)] += 1
+                else:
+                    q = (hours[i] - 8) / 2
+                    if hours[i] % 2 == 0:
+                        self.base_table[day[0]][int(q)] += 0.25
+                    else:
+                        self.base_table[day[0]][int(q)] += 0.75
+                day.pop(0)
+            return True
+        else:
+            return False
+
+
+    def pipline(self, title):
+        title = self.standardize_persian_text(title)
+        title_edit = self.trim_query(title)
+        title_edit = self.convert_day_to_int(title_edit)
+        title_hour = self.time_series(title_edit)
+        if self.tick_base_table(title_edit, title_hour):
+            return True
+        return False
+
+
+    def selector(self, class_list, i=0, selected=None):
+
+        if selected is None:
+            selected = []
+
+        index_class = list(class_list.keys())
+
+        if i >= len(index_class):
+            dt_temp = pd.DataFrame(self.base_table)
+
+
+            selected = ((data[0], data[1][0], data[1][1], data[1][2])for data in selected)
+
+            dt_temp_course = pd.DataFrame(selected, columns=['Course', 'Schedule', 'Score', 'Name_pro'])
+            dt_temp['id'] = self.c + 1
+            dt_temp_course['id'] = self.c + 1
+            self.c += 1
+            self.df_matrix = pd.concat([self.df_matrix, dt_temp])
+            self.df_matrix_course = pd.concat([self.df_matrix_course, dt_temp_course])
+
+            return
+
+        for j in range(len(class_list[index_class[i]])):
+            base_table_copy = copy.deepcopy(self.base_table)
+            selected_copy = selected.copy()
+
+            flag = self.pipline(class_list[index_class[i]][j])
+
+            if flag:
+                selected_copy.append((index_class[i], class_list[index_class[i]][j]))
+                self.selector(class_list, i + 1, selected_copy)
+
+            self.base_table = base_table_copy
+
+
+    def index_trim(self, indx):
+        index_edit = []
+        for i in indx:
+            index_edit.append(i.replace('?', 'ی'))
+        return index_edit
+
+
+    def fitness_function(self, matrx, mean_score_mtrx):
+        val_base = [(0.25, 0.75), (0.75, 0.25)]
+        fitness_scores_list = []
+        for i in val_base:
+
+            matrix_copy = copy.deepcopy(matrx)
+
+            matrix_copy = self.edd_oven(matrix_copy, i[0], i[1])
+            days_present = np.sum(np.any(matrix_copy > 0, axis=1))
+            gaps = 0
+
+            for row in range(matrix_copy.shape[0]):
+                class_indices = np.nonzero(matrix_copy.loc[row])[0]
+                if len(class_indices) > 1:
+                    for k in range(len(class_indices) - 1):
+                        gaps += (class_indices[k + 1] - class_indices[k] - 1)
+
+            fitness = self.day_coff * days_present + self.gap_coff * gaps + self.pr_coff * mean_score_mtrx
+            fitness_scores_list.append(fitness)
+
+
+        fitness_score = np.array(fitness_scores_list).mean()
+        return fitness_score
+
+    @staticmethod
+    def edd_oven(matrx, one_val, zero_val):
+        matrx[matrx == one_val] = 1
+        matrx[matrx == zero_val] = 0
+        return matrx
+
+
+    def evaluate_matrix(self):
+        point_list = []
+        id_table = 0
+        for table in range(0, self.df_matrix.shape[0], 6):
+            id_table += 1
+            mean_score_local_matrix = self.df_matrix_course[self.df_matrix_course['id'] == id_table]['Score'].mean()
+
+            point = self.fitness_function(self.df_matrix.iloc[table:table + 6, :6], mean_score_local_matrix)
+            point_list.append((id_table, point))
+        point_list.sort(key=lambda x: x[1])
+        return np.array(point_list)
+
+    def print_matrix(self, srt_list):
+        programs = []
+        for i, program_id in enumerate(srt_list[:, 0], start=1):
+            messages = []
+            messages.append('*/' * 25)
+            messages.append(f"برنامه {i}: {program_id}")
+            messages.append('*/' * 25)
+            # حداقل جدول
+            minimum_table = self.df_matrix[self.df_matrix['id'] == program_id].iloc[:, :6]
+            minimum_table.index = ['SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
+            minimum_table.columns = ['"8-10', '"10-12', '"12-14', '"14-16', '"16-18', '"18-20']
+            messages.append(minimum_table.to_string())
+            # جزئیات دوره‌ها
+            course = self.df_matrix_course[self.df_matrix_course['id'] == program_id][
+                ['Course', 'Schedule', 'Score', 'Name_pro']]
+            for detail in course.itertuples():
+
+                messages.append(f"Course: {detail.Course}")
+                messages.append(f"Time: {detail.Schedule}")
+                messages.append(f"Score: {detail.Score}")
+                messages.append(f"Name Pro: {self.standardize_persian_text(detail.Name_pro, flag=False)}")
+                messages.append('-'*10)
+            programs.append('\n'.join(messages))
+        return programs
+
+    def all_model(self, courses, id):
+        filtered_rows = pd.DataFrame()
+
+        for _, record in courses.iterrows():
+            course = record['Course']
+            name_pro = record['Name_pro']
+
+            filtered_dt = self.dt[(self.dt['درس'] == course) & (self.dt['استاد'] == name_pro)]
+            filtered_rows = pd.concat([filtered_rows, filtered_dt], ignore_index=True)
+
+        new_unit_selection = UnitSelection(filtered_rows, flag=True, id=id)
+        return new_unit_selection.print_matrix(new_unit_selection.evaluate_matrix())
+
+
+dt = pd.read_csv('Book2.csv', encoding='windows-1256')
+
+
+
+unit_select_obj = UnitSelection(dt)
+
+end_time = time.time()
+print(end_time-srt_time)
---
+++ main_bot.py
@@ -0,0 +1,245 @@
+import os
+import pandas as pd
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+from g import UnitSelection  # جایگزین با نام فایل شما
+
+# توکن بات تلگرام
+TOKEN = '7805910107:AAGOnv683DmFwzTkphdr--BJB7U-QaqPIqM'
+
+# مسیر فایل CSV
+CSV_FILE_PATH = 'Book.csv'
+
+ADMIN_USER_ID = 439165916  # شناسه کاربری تلگرام خود را اینجا قرار دهید
+
+
+# ذخیره وضعیت کاربران
+user_states = {}
+
+# ارسال فایل CSV
+async def send_csv(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    with open(CSV_FILE_PATH, 'rb') as file:
+        await update.message.reply_document(document=file, filename=os.path.basename(CSV_FILE_PATH))
+
+
+async def active_users_count(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_id = update.message.from_user.id
+    if user_id != ADMIN_USER_ID:
+        await update.message.reply_text("شما اجازه دسترسی به این کامند را ندارید.")
+        return
+
+    user_count = len(user_states)
+    await update.message.reply_text(f"تعداد کاربران فعال: {user_count}")
+
+
+async def receive_csv(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    document = update.message.document
+    if document:
+        new_file = await document.get_file()
+        await new_file.download_to_drive('received_file.csv')
+        await update.message.reply_text("فایل دریافت شد. در حال پردازش ...")
+        await process_csv('received_file.csv', update)
+
+
+# پردازش فایل CSV با استفاده از UnitSelection
+async def process_csv(file_path: str, update: Update) -> None:
+    try:
+        # خواندن داده‌ها از فایل CSV
+        df = pd.read_csv(file_path, encoding='windows-1256')
+        user_id = update.message.from_user.id
+
+        # بررسی و تنظیم مقدار پیش‌فرض برای کاربر در user_states
+        if user_id not in user_states:
+            user_states[user_id] = {
+                "method": 1,  # متد پیش‌فرض
+                "day_coff": 10,
+                "gap_coff": 10,
+                "pr_coff": 10
+            }
+
+        # دریافت مقادیر از user_states
+        method = user_states[user_id]["method"]
+        day_coff = user_states[user_id]["day_coff"]
+        gap_coff = user_states[user_id]["gap_coff"]
+        pr_coff = user_states[user_id]["pr_coff"]
+
+        # ایجاد شیء UnitSelection
+        unit_select_obj = UnitSelection(df, method=method, day_coff=day_coff, gap_coff=gap_coff, pr_coff=pr_coff)
+
+        # پردازش داده‌ها و دریافت لیست برنامه‌ها
+        sorted_list = unit_select_obj.evaluate_matrix()
+        programs = unit_select_obj.print_matrix(sorted_list)
+
+        # ذخیره برنامه‌ها و شیء UnitSelection برای کاربر
+        user_states[user_id].update({
+            "programs": programs,
+            "current_index": 1,
+            "unit_select_obj": unit_select_obj
+        })
+
+        # ارسال اولین برنامه به کاربر
+        await update.message.reply_text(f"برنامه اول:\n{programs[0]}")
+    except Exception as e:
+        await update.message.reply_text(f"خطا در پردازش فایل: {e}")
+
+
+async def send_next_program(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_id = update.message.from_user.id
+    if user_id not in user_states or "programs" not in user_states[user_id]:
+        await update.message.reply_text("ابتدا فایل CSV خود را ارسال کنید تا برنامه‌ای ایجاد شود.")
+        return
+
+    state = user_states[user_id]
+    programs = state["programs"]
+    current_index = state["current_index"]
+
+    if current_index < len(programs):
+        # ارسال برنامه فعلی
+        await update.message.reply_text(f"برنامه {current_index + 1}:\n{programs[current_index]}")
+        # بروزرسانی شاخص
+        user_states[user_id]["current_index"] += 1
+    else:
+        await update.message.reply_text("برنامه دیگری وجود ندارد!")
+
+
+# فرمان start
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(
+        'سلام! لطفاً در ابتدا با دستور /setmethod، متود موردنظر را انتخاب کرده و یا فایل CSV خود را مستقیما ارسال کنید.\n'
+        'Ex: \n/setmethod 1  \n/setmethode 2 day_coff gap_coff pr_coff')
+
+
+async def search_more(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_id = update.message.from_user.id
+    username = update.message.from_user.username or f"user_{user_id}"
+
+    # بررسی وجود اطلاعات برای کاربر
+    if user_id not in user_states or "programs" not in user_states[user_id]:
+        await update.message.reply_text("ابتدا فایل CSV خود را ارسال کنید تا برنامه‌ای ایجاد شود.")
+        return
+
+    state = user_states[user_id]
+    programs = state["programs"]
+    current_index = state["current_index"]
+
+    # بررسی برنامه فعلی
+    if current_index - 1 < 0 or current_index - 1 >= len(programs):
+        await update.message.reply_text("برنامه معتبری برای جستجوی بیشتر یافت نشد.")
+        return
+
+    unit_select_obj = state["unit_select_obj"]
+    courses = unit_select_obj.df_matrix_course[unit_select_obj.df_matrix_course['id'] == current_index]
+
+    try:
+        # تولید فایل جدید با استفاده از all_model
+        results_file = unit_select_obj.all_model(courses, current_index)
+        if isinstance(results_file, list):
+            results_file = pd.DataFrame(results_file)
+
+        # تنظیم نام فایل بر اساس نام کاربری
+        file_name = f"@{username}-{current_index}-all-situations.csv"
+
+        # ذخیره فایل
+        file_path = f"./{file_name}"
+        results_file.to_csv(file_path, index=False, encoding="utf-8")
+
+        # ارسال فایل به کاربر
+        await update.message.reply_document(document=open(file_path, 'rb'), filename=file_name)
+        await update.message.reply_text(f"فایل {file_name} آپلود شد.")
+    except Exception as e:
+        await update.message.reply_text(f"خطا در جستجوی بیشتر: {e}")
+
+
+async def set_method(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_id = update.message.from_user.id
+
+    # بررسی وجود آرگومان‌ها
+    if len(context.args) < 1:
+        await update.message.reply_text(
+            "لطفاً متد مورد نظر را با فرمت: /setmethod [1 یا 2] [day_coff] [gap_coff] [pr_coff] وارد کنید.")
+        return
+
+    try:
+        # استخراج متد
+        method = int(context.args[0])
+        if method == 1:
+            # متد 1 باید فقط یک آرگومان داشته باشد
+            if len(context.args) != 1:
+                raise ValueError("متد 1 فقط باید یک آرگومان داشته باشد.")
+
+            day_coff = 10
+            gap_coff = 10
+            pr_coff = 10
+
+            # ذخیره متد انتخابی
+            if user_id not in user_states:
+                user_states[user_id] = {}
+            user_states[user_id].update({
+                "method": method,
+                "day_coff": day_coff,
+                "gap_coff": gap_coff,
+                "pr_coff": pr_coff
+            })
+            await update.message.reply_text(f"متد 1 با موفقیت انتخاب شد. فایل csv خود را آپلود کنید")
+
+        elif method == 2:
+            # متد 2 باید دقیقاً چهار آرگومان داشته باشد
+            if len(context.args) != 4:
+                raise ValueError("متد 2 باید با چهار عدد تنظیم شود: /setmethod 2 [day_coff] [gap_coff] [pr_coff]")
+
+            # استخراج مقادیر day_coff, gap_coff, pr_coff
+            day_coff = int(context.args[1])
+            gap_coff = int(context.args[2])
+            pr_coff = int(context.args[3])
+
+            # ذخیره متد و مقادیر در user_states
+            if user_id not in user_states:
+                user_states[user_id] = {}
+            user_states[user_id].update({
+                "method": method,
+                "day_coff": day_coff,
+                "gap_coff": gap_coff,
+                "pr_coff": pr_coff
+            })
+            await update.message.reply_text(
+                f"متد 2 با مقادیر day_coff={day_coff}, gap_coff={gap_coff}, pr_coff={pr_coff} با موفقیت انتخاب شد. لطفا فایل csv خود را آپلود کنید.")
+
+        else:
+            raise ValueError("عدد وارد شده برای متد معتبر نیست. لطفاً عدد 1 یا 2 را وارد کنید.")
+
+    except ValueError as e:
+        # مدیریت خطاهای مقدار نامعتبر
+        await update.message.reply_text(f"خطا: {e}")
+
+
+async def end(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_id = update.message.from_user.id
+
+    if user_id in user_states:
+        del user_states[user_id]  # حذف وضعیت کاربر از دیکشنری
+        await update.message.reply_text("بات متوقف شد. برای شروع دوباره دستور /start را وارد کنید.")
+    else:
+        await update.message.reply_text("شما هیچ فعالیت فعالی با بات ندارید.")
+
+
+# تابع main
+def main():
+    # ایجاد برنامه
+    application = Application.builder().token(TOKEN).build()
+
+    # افزودن هندلرها
+
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("setmethod", set_method))
+    application.add_handler(CommandHandler("getcsv", send_csv))
+    application.add_handler(CommandHandler("next", send_next_program))
+    application.add_handler(CommandHandler("search", search_more))
+    application.add_handler(CommandHandler("end", end))
+    application.add_handler(MessageHandler(filters.Document.ALL, receive_csv))
+    application.add_handler(CommandHandler("activeusers", active_users_count))
+
+    application.run_polling()
+
+
+if __name__ == '__main__':
+    main()
